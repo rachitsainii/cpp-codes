c27785a64fdead46af92cb77145802ffe689b9b5
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2022-10-17 20:20:47 +0530

    Add files via upload
    I've Commited changes in Binary.cpp :).

---
+++ Binary.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binary_search(int arr[], int first, int last, int key)
+{
+ if (last >= first) {
+ int mid = first + (last - first) / 2;
+ if (arr[mid] == key)
+ return mid;
+ if (arr[mid] > key)
+ return binary_search(arr, first, mid - 1, key);
+ return binary_search(arr, mid + 1, last, key);
+ }
+
+ return -1;
+}
