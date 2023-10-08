1da10c49154d3df48ced71c760799d91c77d268c
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2023-10-09 03:37:46 +0530

    Update BinarySearch.cpp
    Added iterative code for binary search

--- Binary_Search/BinarySearch.cpp
+++ Binary_Search/BinarySearch.cpp
@@ -1,5 +1,6 @@
 #include "BinarySearch.h"
 
+//Recursive Function
 int binary_search(int arr[], int first, int last, int key)
 {
   if (last >= first)
@@ -11,5 +12,23 @@ int binary_search(int arr[], int first, int last, int key)
       return binary_search(arr, first, mid - 1, key);
     return binary_search(arr, mid + 1, last, key);
   }
+  return -1;
+}
+
+// Iterative Function
+int binarySearch(int arr[], int n, int key){
+  int low = 0, high = n-1;
+  while(low <= high){
+    int mid = low + (high-low)/2;
+
+    if(arr[mid] == key){
+      return mid;
+    } else if (arr[mid] > key) {
+      high = mid-1;
+    } else {
+      low = mid+1;
+    }
+  }
+
   return -1;
 }
